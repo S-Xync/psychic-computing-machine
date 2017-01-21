5191b3688a94d2c7d94dd6853d57b0398218a012
SaiKumar Immadi <immadisaikumar@ymail.com>
2017-01-21 14:11:31 +0530

    create tables also drops database at the end until work completes

--- create_tables.php
+++ create_tables.php
@@ -9,26 +9,35 @@ $conn=mysqli_connect($server,$username,$password);
 
 //checks connection
 if(!$conn){
-  die("Connection Failed : ".mysqli_connect_error());
+  die("Connection Failed : ".mysqli_connect_error()."\r\n");
 }
-echo "Connected Successfully using username : ".$username;
+echo "Connected Successfully using username : ".$username."\r\n";
 
 //creates database
 $sql="CREATE DATABASE ".$dbname;
 if(mysqli_query($conn,$sql)){
-  echo "Database : ".$dbname." created successfully";
+  echo "Database : ".$dbname." created successfully\r\n";
 }else{
-  echo "Error creating database : ".$dbname." ".mysqli_error($conn);
+  echo "Error creating database : ".$dbname." ".mysqli_error($conn)."\r\n";
 }
 
 //connects to the database created above
 $sql="USE ".$dbname;
 if(mysqli_query($conn,$sql)){
-  echo "Connected to database : ".$dbname." with user : ".$username;
+  echo "Connected to database : ".$dbname." with user : ".$username."\r\n";
 }else{
-  echo "Error connecting to database : ".$dbname." with user : ".$username." ".mysqli_error($conn);
+  echo "Error connecting to database : ".$dbname." with user : ".$username." ".mysqli_error($conn)."\r\n";
+}
+
+//drops database
+$sql="DROP DATABASE ".$dbname;
+if(mysqli_query($conn,$sql)){
+  echo "Dropped database : ".$dbname." successfully\r\n";
+}else{
+  echo "Error dropping database : ".$dbname." ".mysqli_error($conn)."\r\n";
 }
 
 //closes connection
 mysqli_close($conn);
+ehco "Succesfully Closed MYSQL Connection";
 ?>
