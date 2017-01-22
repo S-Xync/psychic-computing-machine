c182316cf81bb14252c291bacf74bc7b74ba0603
SaiKumar Immadi <immadisaikumar@ymail.com>
2017-01-22 19:00:14 +0530

    some changes

--- create_tables.php
+++ create_tables.php
@@ -30,10 +30,95 @@ if(mysqli_query($conn,$sql)){
 }
 
 //creates tables
+
+//names
 $tbname="names";
 $sql="CREATE TABLE ".$tbname."(
+  id int AUTO_INCREMENT,
+  name varchar(255) NOT NULL UNIQUE,
+  PRIMARY KEY(id)
+)";
+if(mysqli_query($conn,$sql)){
+  echo "Table : ".$tbname." created successfully\r\n";
+}else{
+  echo "Table : ".$tbname." not created ".mysqli_error($conn)."\r\n";
+}
+
+//green_ranks
+$tbname="green_ranks";
+$sql="CREATE TABLE ".$tbname."(
+  id int NOT NULL UNIQUE,
+  green_rank int NOT NULL UNIQUE,
+  CHECK(green_rank>0),
+  FOREIGN KEY (id) REFERENCES names(id)
+)";
+if(mysqli_query($conn,$sql)){
+  echo "Table : ".$tbname." created successfully\r\n";
+}else{
+  echo "Table : ".$tbname." not created ".mysqli_error($conn)."\r\n";
+}
+
+//ranks
+$tbname="ranks";
+$sql="CREATE TABLE ".$tbname."(
+  id int NOT NULL UNIQUE,
+  rank int NOT NULL UNIQUE,
+  CHECK(rank>0),
+  FOREIGN KEY (id) REFERENCES names(id)
+)";
+if(mysqli_query($conn,$sql)){
+  echo "Table : ".$tbname." created successfully\r\n";
+}else{
+  echo "Table : ".$tbname." not created ".mysqli_error($conn)."\r\n";
+}
+
+//previous_ranks
+$tbname="previous_ranks";
+$sql="CREATE TABLE ".$tbname."(
+  id int NOT NULL UNIQUE,
+  previous_rank int,
+  CHECK(previous_rank>0),
+  FOREIGN KEY (id) REFERENCES names(id)
+)";
+if(mysqli_query($conn,$sql)){
+  echo "Table : ".$tbname." created successfully\r\n";
+}else{
+  echo "Table : ".$tbname." not created ".mysqli_error($conn)."\r\n";
+}
+
+//first_appearances
+$tbname="first_appearances";
+$sql="CREATE TABLE ".$tbname."(
+  id int NOT NULL UNIQUE,
+  first_appearance int NOT NULL,
+  CHECK(first_appearance>0),
+  FOREIGN KEY (id) REFERENCES names(id)
+)";
+if(mysqli_query($conn,$sql)){
+  echo "Table : ".$tbname." created successfully\r\n";
+}else{
+  echo "Table : ".$tbname." not created ".mysqli_error($conn)."\r\n";
+}
+
+//first_ranks
+$tbname="first_ranks";
+$sql="CREATE TABLE ".$tbname."(
+  id int NOT NULL UNIQUE,
+  first_rank int NOT NULL,
+  CHECK(first_rank>0),
+  FOREIGN KEY (id) REFERENCES names(id)
+)";
+if(mysqli_query($conn,$sql)){
+  echo "Table : ".$tbname." created successfully\r\n";
+}else{
+  echo "Table : ".$tbname." not created ".mysqli_error($conn)."\r\n";
+}
+
+//computer_names
+$tbname="computer_names";
+$sql="CREATE TABLE ".$tbname."(
   cid int AUTO_INCREMENT,
-  cname varchar(255) NOT NULL UNIQUE,
+  cname varchar(255) NOT NULL,
   PRIMARY KEY(cid)
 )";
 if(mysqli_query($conn,$sql)){
@@ -41,6 +126,14 @@ if(mysqli_query($conn,$sql)){
 }else{
   echo "Table : ".$tbname." not created ".mysqli_error($conn)."\r\n";
 }
+
+//computers
+$tbname="computers";
+$sql="CREATE TABLE ".$tbname."(
+  id int NOT NULL UNIQUE
+)";
+
+
 
 //drops database
 $sql="DROP DATABASE ".$dbname;
